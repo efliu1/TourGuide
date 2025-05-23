2bff1c1aa385034731b17037ac105e109b4860e2
EL <EL@MBs-MacBook-Pro-2.local>
2025-05-23 16:37:01 -0400

    target api 35 instead of 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "ca.tourguide"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
