844ec988e87443b75973be8054b703d150ed4f1e
EL <EL@MBs-MacBook-Pro-2.local>
2025-05-28 22:54:48 -0400

    set up additional screens

---
+++ app/src/main/java/ca/tourguide/ui/create/CreateViewModel.kt
@@ -0,0 +1,13 @@
+package ca.tourguide.ui.create
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CreateViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is create Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/ca/tourguide/ui/favourites/FavouriteFragment.kt
@@ -0,0 +1,42 @@
+package ca.tourguide.ui.favourites
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ca.tourguide.databinding.FragmentFavouriteBinding
+
+class FavouriteFragment : Fragment() {
+
+    private var _binding: FragmentFavouriteBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val accountViewModel =
+            ViewModelProvider(this).get(FavouriteViewModel::class.java)
+
+        _binding = FragmentFavouriteBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textFavourite
+        accountViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
