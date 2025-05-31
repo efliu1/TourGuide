e400164220d19d683a7c2d4a6396049f0566011e
EL <efq.liu@gmail.com>
2025-05-31 09:00:14 -0400

    add auth dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,39 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(FileInputStream(localPropertiesFile))
+}
+
+
+val googleClientId: String = localProperties.getProperty("GOOGLE_CLIENT_ID")
+    ?: throw GradleException("GOOGLE_CLIENT_ID not found in local.properties")
 
 android {
     namespace = "ca.tourguide"
     compileSdk = 35
 
+    buildFeatures{
+        buildConfig = true
+    }
+
     defaultConfig {
         applicationId = "ca.tourguide"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "GOOGLE_CLIENT_ID", "\"$googleClientId\"")
     }
 
     buildTypes {
@@ -39,7 +58,9 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.play.services.auth)
+    implementation("com.google.firebase:firebase-bom:33.14.0")
+    implementation(libs.coil)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -48,6 +69,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.googleid)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
